f6b9e4353294240ccc4dd10a9207f4c02341cc6a
tillchen <ti.chen@jacobs-university.de>
2019-12-05 15:30:18 +0100

    Initial Commit

---
+++ JStore/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  JStore
+//
+//  Created by Till Chen on 12/5/19.
+//  Copyright © 2019 Tianyao Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JStore/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  JStore
+//
+//  Created by Till Chen on 12/5/19.
+//  Copyright © 2019 Tianyao Chen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
