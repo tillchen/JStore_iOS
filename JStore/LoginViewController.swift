0dc58e47a33220bc8c4e032a2adc92995740ef21
tillchen <ti.chen@jacobs-university.de>
2019-12-24 18:04:20 +0100

    added handleUsername

--- JStore/LoginViewController.swift
+++ JStore/LoginViewController.swift
@@ -11,10 +11,18 @@ import UIKit
 class LoginViewController: UIViewController {
     
     let TAG = "LoginViewController"
+    let ADMIN = "tillchen417@gmail.com"
 
     @IBOutlet var textView: UITextView!
-    
     @IBOutlet var textView2: UITextView!
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var sendLinkButton: UIButton!
+    @IBOutlet var anonymousButton: UIButton!
+    
+    var mUsername: String = ""
+    var mValidUsername: Bool = false
+    var mEmail: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +31,39 @@ class LoginViewController: UIViewController {
 
     }
 
+    @IBAction func onSendLinkClicked(_ sender: Any) {
+        
+        handleUsername()
+        
+    }
+    
+    
+    @IBAction func onAnonymousClicked(_ sender: Any) {
+        
+    }
+    
+    func handleUsername() {
+        mUsername = textField.text!
+        if mUsername.isEmpty {
+            showAlert("Your email can't be empty.")
+            mValidUsername = false
+            return
+        }
+        if (mUsername == ADMIN) { // admin mode
+            mEmail = mUsername;
+            mValidUsername = true
+            return
+        }
+        if mUsername.contains(" ") || mUsername.contains("@") {
+            showAlert("You Jacobs username must contain no space and no @, (e.g. tichen).")
+            mValidUsername = false
+        }
+        else {
+            mEmail = mUsername + "@jacobs-university.de";
+            mValidUsername = true
+        }
+    }
+    
     func setHyperLink() {
         let attributedString1 = NSMutableAttributedString(string: "Made with ❤️ by Tianyao Chen and Taiyr Begeyev")
         attributedString1.addAttribute(.link, value: "mailto:tillchen417@gmail.com", range: NSRange(location: 15, length: 13))
@@ -37,6 +78,14 @@ class LoginViewController: UIViewController {
         textView2.attributedText = attributedString2
         textView2.textColor = .lightGray
         textView2.font = UIFont.systemFont(ofSize: 10.0)
+    }
+    
+    func showAlert(_ content: String) {
+        let alertController = UIAlertController(title: "JStore", message:
+            content, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK!", style: .default))
+
+        self.present(alertController, animated: true, completion: nil)
     }
     
     /*
