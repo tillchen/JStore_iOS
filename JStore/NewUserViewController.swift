ce230940b95de254d46ac5837be0e2ed8ec352dd
tillchen <ti.chen@jacobs-university.de>
2019-12-29 14:48:50 +0100

    Built UI for NewUserViewController

---
+++ JStore/NewUserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewUserViewController.swift
+//  JStore
+//
+//  Created by Till Chen on 12/29/19.
+//  Copyright © 2019 Tianyao Chen. All rights reserved.
+//
+
+import UIKit
+
+class NewUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
