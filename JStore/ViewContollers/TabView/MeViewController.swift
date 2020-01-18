8865697ad6ebe0c9a24c2add1d07f31b96effd74
tillchen <ti.chen@jacobs-university.de>
2020-01-18 19:55:59 +0100

    enabled dismissing the keyboard

--- JStore/ViewContollers/TabView/MeViewController.swift
+++ JStore/ViewContollers/TabView/MeViewController.swift
@@ -131,3 +131,15 @@ class MeViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
     }
 
 }
+
+extension MeViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool { // dismiss the keyboard when pressing return
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) { // dismiss the keyboard when touching
+        super.touchesEnded(touches, with: event)
+        view.endEditing(true)
+    }
+}
