e28949596a18579530056869f4c130eb0550f13b
tillchen <ti.chen@jacobs-university.de>
2020-01-18 17:49:33 +0100

    updated MeViewController

--- JStore/ViewContollers/TabView/MeViewController.swift
+++ JStore/ViewContollers/TabView/MeViewController.swift
@@ -11,9 +11,20 @@ import Firebase
 class MeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet var mTableView: UITableView!
+    @IBOutlet var mFullNameTextField: UITextField!
+    @IBOutlet var mDateLabel: UILabel!
+    @IBOutlet var mContactSegmentedControl: UISegmentedControl!
+    @IBOutlet var mPlusSignLabel: UILabel!
+    @IBOutlet var mPhoneTextField: UITextField!
+    @IBOutlet var mLoadActivityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet var mSaveActivityIndicatorView: UIActivityIndicatorView!
     
     let mImages = [UIImage(systemName: "doc.circle"), UIImage(systemName: "eurosign.circle"), UIImage(systemName: "bell.circle")]
     let mData = ["Active Posts", "Sold Items", "Notification Settings"]
+    let WHATSAPP = 0
+    let EMAIL = 1
+    
+    var mWhatsApp: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +35,25 @@ class MeViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
         mTableView.delegate = self
     }
     
-
+    @IBAction func onContactSegmentedControlChanged(_ sender: Any) {
+        switch mContactSegmentedControl.selectedSegmentIndex {
+         case WHATSAPP:
+             mWhatsApp = true
+             mPlusSignLabel.isHidden = false
+             mPhoneTextField.isHidden = false
+         case EMAIL:
+             mWhatsApp = false
+             mPlusSignLabel.isHidden = true
+             mPhoneTextField.isHidden = true
+         default:
+             break
+         }
+    }
+    
+    
+    @IBAction func onSaveClicked(_ sender: Any) {
+    }
+    
     @IBAction func onSignOutClicked(_ sender: Any) {
         do {
             try Auth.auth().signOut()
@@ -34,6 +63,9 @@ class MeViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
             showAlert("Sorry. Something went wrong. Please try again.")
             print(signOutError)
         }
+    }
+    
+    @IBAction func onFeedBackClicked(_ sender: Any) {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
